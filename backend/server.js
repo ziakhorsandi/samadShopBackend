397b629fc9746f52c6569e1544de8e6f31974f7e
zik <ziakhorsandi2@gmail.com>
2021-04-09 22:21:38 +0430

    handle the errors with errorMiddleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import colors from 'colors';
 import connectDB from './config/db.js';
 
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
 import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
@@ -16,6 +18,10 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
