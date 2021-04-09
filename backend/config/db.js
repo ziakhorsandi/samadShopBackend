8556136d8db91383ae920ba6daf38085c04551d7
zik <ziakhorsandi2@gmail.com>
2021-04-09 10:06:12 +0430

    added colors package for more beauties in console messages

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,8 +7,9 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useCreateIndex: true,
     });
+    console.log(`Mogodb Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(`Error: ${error.message}`.red.underline.bold);
     process.exit(1);
   }
 };
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import colors from 'colors';
 import products from './data/products.js';
 import connectDB from './config/db.js';
 
@@ -24,5 +25,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
