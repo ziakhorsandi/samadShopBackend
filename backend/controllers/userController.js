4b151d078c671dd753a1981e90e386967ac90f54
zik <ziakhorsandi2@gmail.com>
2021-05-11 18:08:29 +0430

    getUser and delete user added to user controller, admin added to auth middleWare , proper routes added to user route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -97,4 +97,33 @@ const UpdateUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile, registerUser, UpdateUserProfile };
+//@desc Get all users
+//@route GET /api/users
+//@access Private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
+
+//@desc Delete user
+//@route DELETE /api/users/:id
+//@access Private/Admin
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (user) {
+    await user.remove();
+    res.json({ message: 'User removed' });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+export {
+  authUser,
+  getUserProfile,
+  registerUser,
+  UpdateUserProfile,
+  getUsers,
+  deleteUser,
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,17 +4,21 @@ import {
   getUserProfile,
   registerUser,
   UpdateUserProfile,
+  getUsers,
+  deleteUser,
 } from '../controllers/userController.js';
-import { protect } from '../middleware/authMiddleWare.js';
+import { protect, admin } from '../middleware/authMiddleWare.js';
 
 const router = express.Router();
 
-router.route('/').post(registerUser);
+router.route('/').post(registerUser).get(protect, admin, getUsers);
 router.post('/login', authUser);
 
 router
   .route('/profile')
   .get(protect, getUserProfile)
   .put(protect, UpdateUserProfile);
+
+router.route('/:id').delete(protect, admin, deleteUser);
 
 export default router;
