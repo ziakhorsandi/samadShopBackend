e6d62fc18f55389c8f7dd04e54e1cc0fdedd4ee8
zik <ziakhorsandi2@gmail.com>
2021-05-29 11:32:21 +0430

    added getTopProducts and createProductReview  to productController

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,8 +5,21 @@ import Product from '../models/productModel.js';
 //@route GET /api/products
 //@access Public
 const getProducts = asyncHandler(async (req, res) => {
-  const product = await Product.find({});
-  res.json(product);
+  const pageSzie = 2;
+  const page = Number(req.query.pageNumber) || 1;
+  const keyword = req.query.keyword
+    ? {
+        name: {
+          $regex: req.query.keyword,
+          $options: 'i',
+        },
+      }
+    : {};
+  const count = await Product.countDocuments({ ...keyword });
+  const product = await Product.find({ ...keyword })
+    .limit(pageSzie)
+    .skip(pageSzie * (page - 1));
+  res.json({ product, page, pages: Math.ceil(count / pageSzie) });
 });
 
 //@desc Fetch single  products
@@ -86,10 +99,55 @@ const UpdateProduct = asyncHandler(async (req, res) => {
   }
 });
 
+//@desc Create new review
+//@route POST /api/products/:id/reviews
+//@access Private
+const cerateProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body;
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    let ifExistIndex = -1;
+    ifExistIndex = product.reviews.findIndex(
+      (r) => r.user.toString() === req.user._id.toString()
+    );
+
+    if (ifExistIndex !== -1) {
+      product.reviews.splice(ifExistIndex, 1);
+    }
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+      user: req.user._id,
+      createdAt: Date.now(),
+    };
+    product.reviews.push(review);
+    product.numReviews = product.reviews.length;
+    product.rating =
+      product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      product.reviews.length;
+    const r = await product.save();
+    res.status(201).json(r);
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
+
+//@desc Get top rated products
+//@route GET /api/products/top
+//@access Public
+const getTopProducts = asyncHandler(async (req, res) => {
+  const product = await Product.find({}).sort({ rating: -1 }).limit(3);
+  res.json(product);
+});
+
 export {
   getProducts,
   getProductById,
   deleteProduct,
   createProduct,
   UpdateProduct,
+  cerateProductReview,
+  getTopProducts,
 };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,6 +5,8 @@ import {
   deleteProduct,
   createProduct,
   UpdateProduct,
+  cerateProductReview,
+  getTopProducts,
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleWare.js';
 
@@ -16,11 +18,13 @@ router
   .route('/')
   .get(getProducts)
   .post(protect, admin, upload.single('image'), createProduct);
+router.route('/:id/reviews').post(protect, cerateProductReview);
 router
   .route('/:id')
   .get(getProductById)
   .delete(protect, admin, deleteProduct)
   // .put(protect, admin, UpdateProduct);
   .put(protect, admin, upload.single('image'), UpdateProduct);
+router.get('/top', getTopProducts);
 
 export default router;
